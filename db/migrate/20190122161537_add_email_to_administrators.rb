efa31b26f652e5f74605b5215637c08822d5f758
“shimahi“ <kzrndsk@gmail.com>
2019-01-23 01:35:40 +0900

    herokuのmigrate用にマイグレーションファイルを少し書き換え2

--- db/migrate/20190122161537_add_email_to_administrators.rb
+++ db/migrate/20190122161537_add_email_to_administrators.rb
@@ -1,5 +1,5 @@
 class AddEmailToAdministrators < ActiveRecord::Migration[5.2]
   def change
-    add_column :administrators, :email, :string
+    #add_column :administrators, :email, :string
   end
 end
