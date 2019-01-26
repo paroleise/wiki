d88530deb824bd89d444f488d7dcc5e368fd09dd
“shimahi“ <kzrndsk@gmail.com>
2019-01-26 13:21:19 +0900

    コンテンツのないセクションを非表示に

--- app/helpers/machines_helper.rb
+++ app/helpers/machines_helper.rb
@@ -1,2 +1,10 @@
 module MachinesHelper
+
+  def content_nil?(content)
+    #HACK:: 正規表現まとめる
+    if content.gsub(/<(".*?"|'.*?'|[^'"])*?>/, "").gsub(/^(.+?)[\s　]+(.+)$/, "") == ""
+      "hide"
+    end
+  end
+
 end
