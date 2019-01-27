b6bc79454fd691391c333d046099cc808dd61429
“shimahi“ <kzrndsk@gmail.com>
2019-01-27 20:49:14 +0900

    導入日を導入年月に変更

--- app/helpers/machines_helper.rb
+++ app/helpers/machines_helper.rb
@@ -2,7 +2,7 @@ module MachinesHelper
 
   def content_nil?(content)
     #HACK:: 正規表現まとめる
-    if content.gsub(/<(".*?"|'.*?'|[^'"])*?>/, "").gsub(/^(.+?)[\s　]+(.+)$/, "") == ""
+    if content && content.gsub(/<(".*?"|'.*?'|[^'"])*?>/, "").gsub(/^(.+?)[\s　]+(.+)$/, "") == ""
       "hide"
     end
   end
