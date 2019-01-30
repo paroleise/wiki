958685d636d147a031d9d2f4a05b732b77fcd1ed
“shimahi“ <kzrndsk@gmail.com>
2019-01-31 01:15:15 +0900

    管理者の新規作成を禁止

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,3 +1,8 @@
 class Machine < ApplicationRecord
   has_paper_trail #バージョン管理
+  validates :name, presence: true, uniqueness: true, length: { maximum: 75 }
+  validates :kana, presence: true, uniqueness: true, length: { maximum: 75 }
+  validates :manufacturer, presence: true
+  validates :archetype, presence: true
+  validates :spec, presence: true
 end
